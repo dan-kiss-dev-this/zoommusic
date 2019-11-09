d10a415ff21fdeb7953671c0b7e3924ad73e2cc5
Dan Kiss <flowfast47@gmail.com>
2019-11-08 18:07:20 -0800

    make base app

---
+++ src/Artist.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class Artist extends Component{
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div>sup</div>
+    );
+
+  }
+}
