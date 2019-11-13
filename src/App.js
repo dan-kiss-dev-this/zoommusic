72a109a0f3457b7a677a5b53be6d2af38ef02bdd
Dan Kiss <flowfast47@gmail.com>
2019-11-13 12:17:00 -0800

    add in way to fetch data from api and give loading message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Artist />
+      <Artist name="Mike"/>
     </div>
   );
 }
--- src/Artist.js
+++ src/Artist.js
@@ -1,13 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-export default class Artist extends Component{
+export default class Artist extends Component {
+  state = {
+    artistData: null,
+  }
+
+
+
   componentDidMount() {
-    
+
+
+    let artist = 'eminem'
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artist}`, {
+      headers: {
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+        "x-rapidapi-key": "09d597bf4emshb05af7aa47cbdfdp1f8fe5jsnc7ddf187322b"
+      }
+    })
+      .then(results => results.json())
+      .then(json => this.setState({ artistData: json }))
+
   }
 
   render() {
+    console.log(29, this.state.artistData)
+
     return (
-      <div>sup</div>
+      <div>
+        <div>sup {this.props.name}</div>
+        {this.state.artistData != null ?
+          <div>
+            <ul>
+              {this.state.artistData.data.map(element =>
+
+                <li>{element.title}</li>
+              )}
+            </ul>
+          </div>
+          :
+          <div>Loading...</div>
+        }
+      </div>
     );
 
   }
